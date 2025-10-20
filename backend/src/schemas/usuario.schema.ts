b92cb1a7a242ef72189ed1eecba63b489b0fb129
sandyrodriguesjs <sandyrodriguesdev@gmail.com>
2025-10-20 20:56:35 -0300

    fix: ajustando nomes para se adaptar aos campos da tabela de usuario

--- backend/src/schemas/usuario.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const usuarioSchema = z.object({
-  nome_completo: z.string().min(3, "O nome precisa ter pelo menos 3 caracteres"),
-  email: z.string().email("E-mail inválido"),
-  senha: z.string().min(6, "A senha deve ter no mínimo 6 caracteres"),
-  status: z.string().optional(),
-});
